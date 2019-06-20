612d9f0277651c956d99cd7871b253bd11ca5ee3
gianluca.benanti <gianluca.benanti@gmail.com>
2019-06-20 15:59:08 +0200

    introdotto utilizzo  dei dizionari

--- CodiceFiscale.py
+++ CodiceFiscale.py
@@ -1,9 +1,12 @@
+# inizializzo il dizionario
+Anagrafica = {}
+
 # chiedo di inserire i dati
-Nome = input( "Inserisci il nome: " ).upper()
-Cognome = input( "Inserisci il cognome: " ).upper()
-# Sesso = input("Inserisci il tuo sesso (M per maschio ed F per femmina): ").upper()
-# Luogo = input("Inserisci il tuo luogo di nascita: ")
-# Data = input("Inserisci la tua data di nascita (gg/mm/aaaa): ")
+Anagrafica["Nome"] = input( "Inserisci il nome: " ).upper()
+Anagrafica["Cognome"] = input( "Inserisci il cognome: " ).upper()
+#Anagrafica["Sesso"] = input("Inserisci il tuo sesso (M per maschio ed F per femmina): ").upper()
+#Anagrafica["Luogo"] = input("Inserisci il tuo luogo di nascita: ")
+#Anagrafica["Data"] = input("Inserisci la tua data di nascita (gg/mm/aaaa): ")
 
 # verifico che la data sia corretta
 # if not (Data[:2].isalnum() and Data[3:5].isalnum() and Data[6:10].isalnum()):
@@ -21,10 +24,10 @@ def consonanti(stringa):
 pass
 
 
-b = consonanti( Nome )
+b = consonanti(Anagrafica["Nome"])
 CF_Nome = b[0] + b[2] + b[3]
 
-b = consonanti( Cognome )
+b = consonanti(Anagrafica["Cognome"])
 CF_Cognome = b[0] + b[1] + b[2]
 
 print( CF_Cognome+CF_Nome )
