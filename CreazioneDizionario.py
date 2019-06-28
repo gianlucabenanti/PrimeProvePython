25b5c2104ca3d65d10a98f83acffa1e24c69a174
gianluca.benanti <gianluca.benanti@gmail.com>
2019-06-28 15:33:13 +0200

    Creazione procedura per estrazione dizionario da CSV comuni

--- CreazioneDizionario.py
+++ CreazioneDizionario.py
@@ -1,3 +1,22 @@
-f = open("Elenco-comuni-italiani.CSV","r")
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
-print(f.read(5))
+fname = "C:/Gianluca/PYTHON/Elenco.csv"
+
+f = open(fname,"r")
+
+stringa = f.readlines()
+
+f.close()
+
+dict = {stringa[i][:stringa[i].find(";")]:stringa[i][stringa[i].find(";")+1:] for i in range(0,file_len(fname))}
+
+
+f = open("C:/Gianluca/PYTHON/DizionarioComuni.py","a")
+
+f.write(str(dict))
+
+f.close()
